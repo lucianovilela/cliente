444df43c487bbbb6b137133604d1a101c1a0868e
lucianovilela <lucianovilela@gmail.com>
2024-10-09 14:54:20 +0000

    wip

--- view/app/page.js
+++ view/app/page.js
@@ -5,7 +5,7 @@ export default function Home() {
     <div className="flex justify-center items-center h-screen bg-gray-100">
       <a
         href="/user"
-        className="w-64 h-32 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg shadow-lg hover:shadow-xl flex justify-center items-center transition duration-300 ease-in-out transform hover:-translate-y-1"
+        className="w-64 h-32 bg-green-300 hover:bg-green-600 text-gray-500 font-semibold rounded-lg shadow-lg hover:shadow-xl flex justify-center items-center transition duration-300 ease-in-out transform hover:-translate-y-1"
       >
         <span className="text-lg">Ir para a Página de Usuários</span>
       </a>
