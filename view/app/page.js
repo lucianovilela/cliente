341ec01fb0ec7d8dccd56974e70059f4a133d4ce
lucianovilela <lucianovilela@gmail.com>
2024-10-09 14:46:49 +0000

    wip

--- view/app/page.js
+++ view/app/page.js
@@ -2,8 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <a
+        href="/user"
+        className="w-64 h-32 bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-lg shadow-lg hover:shadow-xl flex justify-center items-center transition duration-300 ease-in-out transform hover:-translate-y-1"
+      >
+        <span className="text-lg">Ir para a Página de Usuários</span>
+      </a>
     </div>
+
   );
 }
